db80af967d345f74e01a8268377eae427338847b
lehoang081191 <lehoang0811@gmail.com>
2021-04-07 23:26:45 +0700

    update

--- WEEK-1/Introduction to Java/TEST/src/test.java
+++ WEEK-1/Introduction to Java/TEST/src/test.java
@@ -1,98 +1,19 @@
-import java.util.Scanner;
-
-public class test {
-//    public static void main(String[] args) {
-//        System.out.println("Bảng cửu chương 5");
-//        for (int i = 1; i <= 10; i++){
-//            int result = 5*i;
-//
-//            System.out.println("5x" + i + "=" + result);
-//        }
-//        for (int i = 2; i<=9; i++){
-//            for(int j=1; j<=10; j++){
-//                System.out.println(i + "x" + j + "=" + i*j);
-//
-//            }
-//        }
-//    }
-//
-//}
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Nhap hinh");
-//        while (true){
-//            int choice = sc.nextInt();
-//            switch (choice) {
-//                case 1:
-//                    System.out.println("Hinh 1");
-//                    System.out.println(" * ");
-//                    System.out.println(" * * ");
-//                    System.out.println(" * * * ");
-//                    System.out.println(" * * * * ");
-//                    System.out.println(" * * * * * ");
-//                    break;
-//                case 2:
-//                    System.out.println("Hinh 2");
-//                    System.out.println(" * * * * *");
-//                    System.out.println(" * * * * ");
-//                    System.out.println(" * * * ");
-//                    System.out.println(" * * ");
-//                    System.out.println(" * ");
-//                    break;
-//            }
-//        }
-//    }
-//}
-//    for (int i = 1; i<=5; i++ ){
-//        for (int j = 1; j<=i; j++){
-//            System.out.print("*");
-//        }
-//        System.out.println();
-//    }
-//    }
-//}
-//        for (int i = 1; i<=5; i++ ){
-//            for (int j = 5; j>=i; j--){
-//            System.out.print(" * ");
-//            }
-//            System.out.println();
-//        }
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter a number");
-//        int n = sc.nextInt();
-//
-//        for (int i = 1; i<=n; i++){
-//            if(i%2==0){
-//                System.out.println(i);
-//            }
-//        }
-//        static int m(int x) {
-//            int b = 0;
-//            for(int a:x){
-//                b+=a;
-//            }
-//            return b;
-//
 
 
-        void method(ChuNhat x) {
-            x.rong = 5;
-            x.dai = 5;
-        }
+public class test{
+    private int id;
+    private String name;
+    private int balance = 0;
 
-        public static void main(String[] args) {
-            test o = new test();
-            ChuNhat cn = new ChuNhat(1, 1);
-            o.method(cn);
-            System.out.printf("x=%d, y=%d", cn.rong, cn.dai);
-        }
-    }
-    class ChuNhat {
-        public int rong, dai;
-
-        public ChuNhat(int rong, int dai) {
-            this.rong = rong;
-            this.dai = dai;
-        }
+    public int getId() {
+        return id;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getBalance() {
+        return balance;
+    }
+}
